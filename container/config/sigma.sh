e4ef3fb13eecffe06c79e51632c39bf9cc914e5a
enriqueesanchz <enrsancar1@alum.us.es>
2024-02-17 13:15:46 +0100

    add config: sigma and wars

---
+++ container/config/sigma.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Strict mode
+set -euo pipefail
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Package name
+package="sigma"
+
+# Config
+db="sigma"
+dbuser="sigma"
+dbpass="sigmadb"
+file="sigmatree.tgz"
+
+# Aux function
+getfromcode()
+{
+    wget --quiet --user ${dbuser} --password ${dbpass} -N "https://everest.us.es/code/$1" -O "/tmp/$1"
+}
+
+# Config
+
+configure() {
+    getfromcode ${file}
+    tar zxf /tmp/sigmatree.tgz -C /
+}
+
+clean() {
+    rm /tmp/${file}
+}
+
+remove() { :; }
+
+trap clean EXIT
+
+main() {
+    configure
+    printf "[${package}] Succesfully configured"
+}
+
+main
+
