8678556480abbaa883e178745534f33542de2c37
enriqueesanchz <enrsancar1@alum.us.es>
2024-02-24 21:36:44 +0100

    add container volumes

---
+++ container/config/config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+chmod +x ./mariadb.sh && ./mariadb.sh
+chmod +x ./apache2.sh && ./apache2.sh
+chmod +x ./wildfly.sh && ./wildfly.sh
+chmod +x ./sigma.sh && ./sigma.sh
+chmod +x ./wars.sh && ./wars.sh
+
