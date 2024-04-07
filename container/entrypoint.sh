e4a7ad733ba4b46a463c57bdae3abd46ec0a6ec9
enriqueesanchz <enrsancar1@alum.us.es>
2024-04-07 23:20:04 +0200

    organize entrypoint

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -24,17 +24,13 @@ for dir in "${dirs[@]}"; do
     fi
 done
 
-
-dirs=("/code")
-
-for dir in "${dirs[@]}"; do
-    if [ -z "$(ls -A ${dir})" ]; then
-        printf "${dir} vacio, clonando quickstart\n" 1>&2
-        git clone https://github.com/wildfly/quickstart.git /code/quickstart
-    else
-        printf "usando volumes${dir}\n" 1>&2
-    fi
-done
+# /code
+if [ -z "$(ls -A /code)" ]; then
+    printf "/code vacio, clonando quickstart\n" 1>&2
+    git clone https://github.com/wildfly/quickstart.git /code/quickstart
+else
+    printf "usando volumes/code\n" 1>&2
+fi
 
 # mysql permission
 chown -R mysql:mysql /var/lib/mysql
