ca7709351dd764a1f47c27b33c2aaecaddbedf05
enriqueesanchz <enrsancar1@alum.us.es>
2024-05-31 19:18:49 +0200

    tigervncpasswd as env terraform

--- container/terraform/main.tf
+++ container/terraform/main.tf
@@ -123,4 +123,10 @@ resource "aws_elastic_beanstalk_environment" "entorno-env" {
     name      = "vpn_password"
     value     = var.vpn_password
   }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:application:environment"
+    name      = "tigervncpasswd"
+    value     = var.tigervncpasswd
+  }
 }
